e48e8ed1e0cfda8698a1110686e575167c135041
Yihong Deng <dyh228@gmail.com>
2018-05-12 09:11:45 -0700

    edit assignment2

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -18,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
-##  If the inverse has already been calculated (and the matrix has not changed),
+## If the inverse has already been calculated (and the matrix has not changed),
 ## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
